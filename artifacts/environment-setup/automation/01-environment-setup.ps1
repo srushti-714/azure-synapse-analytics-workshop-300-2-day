5ab1b1aadda4ac1d24fc90774f3971d24f661c40
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2020-12-02 21:46:23 +0530

    Update 01-environment-setup.ps1

--- artifacts/environment-setup/automation/01-environment-setup.ps1
+++ artifacts/environment-setup/automation/01-environment-setup.ps1
@@ -132,8 +132,11 @@ Set-AzKeyVaultAccessPolicy -ResourceGroupName $resourceGroupName -VaultName $key
 
 #remove need to ask for the password in script.
 #$sqlPasswordSecret = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name "SqlPassword"
-$global:sqlPassword = $(Get-AzKeyVaultSecret -VaultName $keyVaultName -Name "SqlPassword").SecretValueText
+#$global:sqlPassword = $(Get-AzKeyVaultSecret -VaultName $keyVaultName -Name "SqlPassword").SecretValueText
 #$global:sqlPassword = ConvertFrom-SecureString -SecureString $sqlPasswordSecret.SecretValue -AsPlainText #| ConvertFrom-SecureString -AsPlainText
+
+
+$global:sqlPassword = $(Get-AzKeyVaultSecret -VaultName $keyVaultName -Name "SqlPassword").SecretValueText
 
 Write-Information "Create SQL-USER-ASA Key Vault Secret in Key Vault $($keyVaultName)"
 $secretValue = ConvertTo-SecureString $sqlPassword -AsPlainText -Force
