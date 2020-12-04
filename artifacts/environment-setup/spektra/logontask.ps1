e2005a100549eacec95e13137a22b1d9beb41a2d
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2020-12-04 14:40:19 +0530

    Update logontask.ps1

--- artifacts/environment-setup/spektra/logontask.ps1
+++ artifacts/environment-setup/spektra/logontask.ps1
@@ -2,7 +2,10 @@ Start-Transcript -Path C:\WindowsAzure\Logs\logontasklogs.txt -Append
 cd 'C:\LabFiles\synapse-ws-L300\artifacts\environment-setup\automation'
 
 ./01-environment-setup.ps1
-./03-environment-validate.ps1
+./07-01-environment-poc-pre-validate.ps1
+./07-02-environment-poc-validate.ps1
+
+#./03-environment-validate.ps1
 
 Unregister-ScheduledTask -TaskName "Setup" -Confirm:$false
 Stop-Transcript
