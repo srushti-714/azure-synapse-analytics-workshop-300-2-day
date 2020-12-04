7d6a40aff8050c818565b118d15b935d7e67a419
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2020-12-04 14:25:55 +0530

    Update post-install-script01.ps1

--- artifacts/environment-setup/spektra/post-install-script01.ps1
+++ artifacts/environment-setup/spektra/post-install-script01.ps1
@@ -140,7 +140,7 @@ $wclient.DownloadFile($url, $output)
 (Get-Content -Path "c:\LabFiles\parameters.json") | ForEach-Object {$_ -Replace "GET-DEPLOYMENT-ID", "$deploymentId"} | Set-Content -Path "c:\LabFiles\parameters.json"
 
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
-  -TemplateUri "https://raw.githubusercontent.com/solliancenet/azure-synapse-analytics-workshop-400/master/artifacts/environment-setup/automation/00-asa-workspace-core.json" `
+  -TemplateUri "https://raw.githubusercontent.com/solliancenet/azure-synapse-analytics-workshop-300-2-day/master/artifacts/environment-setup/automation/00-asa-workspace-core.json" `
   -TemplateParameterFile "c:\LabFiles\parameters.json"
 
 Uninstall-AzureRm
